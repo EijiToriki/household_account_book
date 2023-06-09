9e7ad6caeec9d5611cb6cb078203b2939b83157c
Eiji Toriki <ageForEngi@outlook.jp>
2023-06-09 23:05:54 +0900

    明細削除とpostのコードを記述

--- frontend/src/components/IncomeRegistration.js
+++ frontend/src/components/IncomeRegistration.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import Grid from '@mui/material/Grid';
 
 import Details from './subcomponents/Details';
@@ -15,6 +16,25 @@ export default function IncomeRegistration() {
       }])
   }
 
+  const handleRegisterDetails = async(e) => {
+    // let baseURL = "http://127.0.0.1:5000/incomeRegister"
+    // // JSON.stringify(details)
+    // try{
+    //   const res = await axios.post(baseURL, JSON.stringify(details))
+    //   console.log(res.data)
+    // }catch(error){
+    //   console.log(error)
+    // } 
+  }
+
+  const handleRemoveDetails = () => {
+    if(details.length > 1){
+      const updatedDetails = [...details];
+      updatedDetails.splice(details.length-1, 1);
+      setDetails(updatedDetails);  
+    }
+  }
+
   const formChar = '収入'
   const [details, setDetails] = useState([{
     registerDate: "",
@@ -23,8 +43,6 @@ export default function IncomeRegistration() {
     comment: ""
   }])
 
-  console.log(details)
-
   return (
     <React.Fragment>
       <Grid container spacing={3}>
@@ -32,11 +50,13 @@ export default function IncomeRegistration() {
           <Grid item xs={6} key={idx}>
             <Details formChar={formChar} detailCount={idx+1} data={data}/>
           </Grid>
-        ))}
-        
+        ))} 
       </Grid>
-      
-      <DetailOpButton handleAddDetails={handleAddDetails} />
+      <DetailOpButton 
+        handleAddDetails={handleAddDetails} 
+        handleRegisterDetails={handleRegisterDetails} 
+        handleRemoveDetails={handleRemoveDetails} 
+      />
     </React.Fragment>
   )
 }
--- frontend/src/components/subcomponents/DetailOpButton.js
+++ frontend/src/components/subcomponents/DetailOpButton.js
@@ -2,12 +2,35 @@ import React from 'react'
 import Grid from '@mui/material/Grid';
 import { Button } from '@mui/material';
 
-export default function DetailOpButton({handleAddDetails}) {
+export default function DetailOpButton({handleAddDetails, handleRegisterDetails, handleRemoveDetails}) {
   return (
     <Grid container alignItems='center' direction='column'>
       <Grid item sx={{ p: 4 }}>
-        <Button variant="outlined" size='large' sx={{ marginRight: 4, width: 150 }} onClick={() => handleAddDetails()} >明細追加</Button>
-        <Button variant="contained" size='large' sx={{ marginLeft: 4, width: 150 }}>登録</Button>
+        <Button 
+          variant="outlined" 
+          size='large' 
+          sx={{ marginRight: 4, width: 150 }} 
+          onClick={() => handleAddDetails()} 
+        >
+          明細追加
+        </Button>
+        <Button 
+          variant="contained" 
+          size='large' 
+          sx={{ marginLeft: 4, marginRight: 4, width: 150 }}
+          onClick={() => handleRegisterDetails()}
+        >
+          登録
+        </Button>
+        <Button 
+          variant="outlined" 
+          size='large' 
+          color='error' 
+          sx={{ marginLeft: 4, width: 150 }} 
+          onClick={() => handleRemoveDetails()}
+        >
+          削除
+        </Button>
       </Grid>
     </Grid>
   )
