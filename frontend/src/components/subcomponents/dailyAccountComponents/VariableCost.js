37979b4e3c09415e8a025f358a9c6221fc958030
Eiji Toriki <ageForEngi@outlook.jp>
2023-07-01 21:33:12 +0900

    更新用モーダルを設置

--- frontend/src/components/subcomponents/dailyAccountComponents/VariableCost.js
+++ frontend/src/components/subcomponents/dailyAccountComponents/VariableCost.js
@@ -4,11 +4,15 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { Typography } from '@mui/material';
+import { Box, Button, Modal, Typography } from '@mui/material';
 
 import Title from '../Title'
 
 export default function VariableCost({variableData}) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <React.Fragment>
       <Title>変動費</Title>
@@ -27,15 +31,30 @@ export default function VariableCost({variableData}) {
               <TableCell><Typography>{colData[0]}</Typography></TableCell>
             {variableData.map((rowData, rowIdx) => (
               <TableCell key={rowIdx}>
-                <Typography>
-                  {rowData[2][colIdx][1]}円
-                </Typography>
+                <Button onClick={handleOpen} variant='text'>
+                  <Typography>
+                    {rowData[2][colIdx][1]}円
+                  </Typography>
+                </Button>
               </TableCell>
             ))}
             </TableRow>
           ))}         
         </TableBody>
       </Table>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4}}>
+          <Typography>
+            ここに更新用情報を記載
+          </Typography>
+        </Box>
+      </Modal>
     </React.Fragment>
   )
 }
